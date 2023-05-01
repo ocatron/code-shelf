a67852b58fa87ba321853641059258f88231ccfe
Thamara Liyanage <thamara.prabuddha@gmail.com>
2023-05-01 17:08:25 +0530

    Enable code block copy button by default

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  defaultShowCopyCode: true,
 });
 
 module.exports = withNextra(nextConfig);
